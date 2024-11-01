907d80add06e092432ba07d50130ae2b5fab6230
PedroPepeu <pedrosteamgtav@gmail.com>
2024-11-01 10:12:02 -0300

    adding inputs

--- home/home.go
+++ home/home.go
@@ -1,26 +1,67 @@
 package home
 
 import (
+	// golang imports
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
+	// github imports
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 
+	// project imports
 	chat "chatip/chat"
 )
 
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
 type homeScreen struct {
-	ip       string
-	own_name string
-	cursor 	 int
+	focusIndex 	 int
+	inputs 		 []textinput.Model
+	cursorMode 	 cursor.Mode
 }
 
 func InitialModel() homeScreen {
-	return homeScreen{
-		ip:       "127.0.0.1",
-		own_name: "default",
+	hs := homeScreen {
+		inputs: make([]textinput.Model, 2),
 	}
+
+	var t textinput.Model
+	for i := range hs.inputs {
+		t = textinput.New()
+		t.Cursor.Style = cursorStyle
+		t.CharLimit = 15
+
+		switch i {
+		case 0:
+			t.Placeholder = "0.0.0.0"
+			t.Focus()
+			t.PromptStyle = focusedStyle
+			t.TextStyle = focusedStyle
+		
+		case 1:
+			t.Placeholder = "Alias"
+			t.CharLimit = 20
+		}
+
+		hs.inputs[i] = t
+	}
+
+	return hs
 }
 
 func (hs homeScreen) Init() tea.Cmd {
@@ -28,37 +69,110 @@ func (hs homeScreen) Init() tea.Cmd {
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 
-	return nil
+	return textinput.Blink
 }
 
 func (hs homeScreen) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+
+		// exit the application
+		case "ctrl+c", "esc":
 			return hs, tea.Quit
 
-		case "up", "k":
-			if hs.cursor > 0 {
-				hs.cursor--
+		// Change cursor mode
+		case "ctrl+r":
+			hs.cursorMode++
+			if hs.cursorMode > cursor.CursorHide {
+				hs.cursorMode = cursor.CursorBlink
+			}
+			cmds := make([]tea.Cmd, len(hs.inputs))
+			for i := range hs.inputs {
+				cmds[i] = hs.inputs[i].Cursor.SetMode(hs.cursorMode)
+			}
+			return hs, tea.Batch(cmds...)
+
+		// Set focus to next input
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
+
+			// Did the user press enter while the submit button was focused?
+			// If so, exit.
+			if s == "enter" && hs.focusIndex == len(hs.inputs) {
+				return hs, tea.Quit
 			}
 
-		case "down", "j":
-			// if hs.cursor < len(hs.choices)-1 {
-			// 	hs.cursor++
-			// }
+			// Cycle indexes
+			if s == "up" || s == "shift+tab" {
+				hs.focusIndex--
+			} else {
+				hs.focusIndex++
+			}
 
+			if hs.focusIndex > len(hs.inputs) {
+				hs.focusIndex = 0
+			} else if hs.focusIndex < 0 {
+				hs.focusIndex = len(hs.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(hs.inputs))
+			for i := 0; i <= len(hs.inputs)-1; i++ {
+				if i == hs.focusIndex {
+					// Set focused state
+					cmds[i] = hs.inputs[i].Focus()
+					hs.inputs[i].PromptStyle = focusedStyle
+					hs.inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				// Remove focused state
+				hs.inputs[i].Blur()
+				hs.inputs[i].PromptStyle = noStyle
+				hs.inputs[i].TextStyle = noStyle
+			}
+
+			return hs, tea.Batch(cmds...)
+		}
+	}
+		// confirm
 		case "enter", " ":
 			return chat.InitialModel().Update(msg)
 			
 		}
+	
+
+	cmd := hs.updateInputs(msg)
+	return hs, cmd
+}
+
+func (hs *homeScreen) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(hs.inputs))
+
+	for i := range hs.inputs {
+		hs.inputs[i], cmds[i] = hs.inputs[i].Update(msg)
 	}
-	return hs, nil
+
+	return tea.Batch(cmds...)
 }
 
 func (hs homeScreen) View() string {
-	s := "Home screen"
-	return s
+	var b strings.Builder
+
+
+	for i := range hs.inputs {
+		b.WriteString(hs.inputs[i].View())
+		if i < len(hs.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+	if hs.focusIndex == len(hs.inputs) {
+		button = &focusedButton
+	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+	return b.String()
 }
 
 func main() {
